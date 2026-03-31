90ca0d9460e182f5dcc92527fcc199d99a8e5174
tanbirJibon202 <tanbirahamed202@gmail.com>
2026-04-01 03:51:03 +0600

    Add lower bound, upper bound and Highest index where < target (binary search)

--- introductionToBinarySearch/highestIndex.cpp
+++ introductionToBinarySearch/highestIndex.cpp
@@ -5,18 +5,16 @@ int main(){
   int arr[] = {1, 2, 3, 3, 5, 7, 9, 9, 9, 10, 12};
   int n = sizeof(arr) / sizeof(arr[0]);
   int left = 0, right = n, target;
+  // HIGHEST INDEX WHERE < TARGET
   cin>> target;
   while (left < right)
   {
     int mid = left + (right - left) / 2;
     // check mid
-    if (arr[mid] >= target)    
-      right = mid;
-    else left = mid + 1;
+    if (arr[mid] < target)    
+      left = mid;
+    else right = mid - 1;
   }
-  cout << "Lower Bound of index: " << left << endl;
+  cout << "Highest Index where < target: " << left << endl;
   return 0;
 }
-
-
-
--- introductionToBinarySearch/upperBound.cpp
+++ introductionToBinarySearch/upperBound.cpp
@@ -1,18 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-  
-  int arr[] = {1, 2, 2, 3, 4, 7, 7, 20, 30, 31};
+  int arr[] = {1, 2, 3, 3, 5, 7, 9, 9, 9, 10, 12};
   int n = sizeof(arr) / sizeof(arr[0]);
   int left = 0, right = n, target;
   cin>> target;
   while (left < right)
   {
     int mid = left + (right - left) / 2;
-    if (arr[mid] <= target)
-      left = mid + 1;
-    else right = mid;
+    if (arr[mid] > target)
+      right = mid;
+    else left = mid + 1;
   }
-  cout << "Upper Bound: " << left << endl;
+  cout << "Upper Bound of index: " << left << endl;
   return 0;
 }
